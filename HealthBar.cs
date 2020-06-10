177fd7415f75d03fb0013a2f1abc81ab5defb730
Quentin PETREQUIN <56532352+Spriinty@users.noreply.github.com>
2020-06-10 17:11:09 +0200

    Add files via upload

---
+++ HealthBar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public Gradient gradient;
+    public Image fill;
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+
+        fill.color = gradient.Evaluate(1f);
+        //La couleur récupérée est à 1 soit 100%, ici le vert
+    }
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+}
